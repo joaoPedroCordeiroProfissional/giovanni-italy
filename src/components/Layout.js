e6f7403d242d31467c4513fa0dbdd76535544673
jotape02 <jpc5626@gmail.com>
2021-06-28 21:42:03 -0300

    Tentativa para fazer o React Helmet funcionar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ import "../styles/global.css"
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
+    <div>
       <Helmet>
         {/* Primary Meta Tags */}
           <meta charSet="utf-8" />
@@ -18,12 +18,12 @@ export default function Layout({ children }) {
           <meta name="title" content="GDF Housing - Rental Solutions" />
           <meta name="description" content="Your trustworthy real estate agent is right here!" />
 
-          {/* Open Graph / Facebook  */}
+          {/* Open Graph / Facebook 
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.gdfhousing.com" />
           <meta property="og:title" content="GDF Housing - Rental Solutions" />
           <meta property="og:description" content="Your trustworthy real estate agent is right here!" />
-          <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+          <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" /> */}
         </Helmet>
       <Header />
       <div className="content">{children}</div>
