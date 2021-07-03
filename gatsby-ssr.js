aa03aa953219ac43d0d75bd26fac837f2120c37b
jotape02 <jpc5626@gmail.com>
2021-07-03 20:56:58 -0300

    adição de um código pre-renderização que muda a tag de estilo de lugar

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,21 @@
-exports.onPreRenderHTML = ({ replaceHeadComponents, getHeadComponents }) => {
-	const headComponents = getHeadComponents();
-	console.log(headComponents);
-	headComponents.forEach(head => {
-		if (head.props && head.props['data-react-helmet']) {
-			delete head.props['data-react-helmet'];
-		}
-	});
-	replaceHeadComponents(headComponents);
+exports.onPreRenderHTML = function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+}) {
+  const headComponents = getHeadComponents();
+  headComponents.sort((a, b) => {
+    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+      return 0;
+    }
+
+    if (a.type === 'style') {
+      return 1;
+    } else if (b.type === 'style') {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  replaceHeadComponents(headComponents);
 };
